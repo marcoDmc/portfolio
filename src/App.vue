f19d1ba33d559b13c03ebe571e95bdd17de141e2
mdev <marcodamasceno07@gmail.com>
2022-06-07 19:55:41 -0300

    :sparkles: added the main component

---
+++ src/App.vue
@@ -0,0 +1,48 @@
+<template>
+  <section>
+    <Header />
+    <Main />
+    <Footer />
+  </section>
+</template>
+
+<script>
+import Header from "./components/HeaderPortfolio.vue";
+import Main from "./components/MainPortfolio.vue";
+import Footer from "./components/FooterPortfolio.vue";
+export default {
+  name: "App",
+  components: {
+    Header,
+    Main,
+    Footer
+  }
+}
+</script>
+
+<style>
+* {
+  padding: 0;
+  margin: 0;
+  box-sizing: border-box;
+  scroll-behavior: smooth;
+
+}
+
+
+
+body {
+  font-family: sans-serif;
+  font-size: 1rem;
+  list-style: none;
+  font-family: 'Titillium Web', sans-serif;
+  overflow-x: none;
+  background-color:var(--blue-dark);
+
+}
+
+a {
+  text-decoration: none;
+
+}
+</style>
