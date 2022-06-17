a2d77937b407ad5cc6bd602709e5fb45b5fa2173
mdev <marcodamasceno07@gmail.com>
2022-06-17 04:49:27 -0300

    :wrench: configured global access to the vueuse/motion animation library

--- src/main.js
+++ src/main.js
@@ -1,10 +1,13 @@
 import { createApp } from "vue";
 import { createPinia } from "pinia";
+import { MotionPlugin } from '@vueuse/motion'
 
 import App from "./App.vue";
 
 const app = createApp(App);
 
 app.use(createPinia());
-
+app.use(MotionPlugin)
 app.mount("#app");
+
+export { app };
