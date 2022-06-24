891a0c7993eddb4c0896238e3d7b2e822f7a4045
mdev <marcodamasceno07@gmail.com>
2022-06-24 03:01:55 -0300

    :wrench: configured vue-tilt library

--- src/main.js
+++ src/main.js
@@ -3,11 +3,13 @@ import { createPinia } from "pinia";
 import { MotionPlugin } from '@vueuse/motion'
 
 import App from "./App.vue";
+import VueTilt from "vue-tilt.js";
 
 const app = createApp(App);
 
 app.use(createPinia());
 app.use(MotionPlugin)
+app.use(VueTilt);
 app.mount("#app");
 
 export { app };
